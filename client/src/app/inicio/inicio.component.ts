d041196b08fc733669b6b95eaf601af19950d76f
Maria <mini.mar@hotmail.com>
2024-07-16 09:56:40 +0200

    adding layout for the bed component

--- client/src/app/inicio/inicio.component.ts
+++ client/src/app/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
